e13eabaca3baa9b73c281f219ae2f6b3ea49817b
willfield <52052049+willfield99@users.noreply.github.com>
2020-10-26 09:32:25 -0400

    z

---
+++ ClickCell.java
@@ -0,0 +1,25 @@
+
+/*4/16/19
+ * 
+ * ClickCell Handles the mouse event for toggling a cell
+ * 
+ */
+import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
+import java.awt.event.MouseAdapter;
+
+public class ClickCell extends MouseAdapter {
+
+	private TPanel panel;// panel
+
+	public ClickCell(TPanel panel) throws FileNotFoundException {// ClickCell constructor
+
+		this.panel = panel;
+	}
+
+	public void mouseClicked(MouseEvent event) {// gets The event where the mouse is clicked and toggles the cell there
+		if (MouseEvent.BUTTON1 == event.getButton()) {
+			panel.toggleCellAt(event.getX(), event.getY());// toggling cell where the mouse is clicked
+		}
+	}
+}
